88a5550085094c99918c2df12f36bb8411edd552
TeslaAngel <mohadot@outlook.com>
2026-05-07 12:08:47 -0700

    Enabled handtracking

---
+++ Assets/Scripts/HandGestureDebugLogger.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class HandGestureDebugLogger : MonoBehaviour
+{
+    private bool leftFist;
+    private bool rightFist;
+
+    private bool leftThumbUp;
+    private bool rightThumbUp;
+
+    public void OnLeftFistPerformed()
+    {
+        leftFist = true;
+        Debug.Log("[Hand Gesture] Left fist detected");
+
+        CheckBothFists();
+    }
+
+    public void OnLeftFistEnded()
+    {
+        leftFist = false;
+        Debug.Log("[Hand Gesture] Left fist ended");
+    }
+
+    public void OnRightFistPerformed()
+    {
+        rightFist = true;
+        Debug.Log("[Hand Gesture] Right fist detected");
+
+        CheckBothFists();
+    }
+
+    public void OnRightFistEnded()
+    {
+        rightFist = false;
+        Debug.Log("[Hand Gesture] Right fist ended");
+    }
+
+    public void OnLeftFistThumbUpPerformed()
+    {
+        leftThumbUp = true;
+        Debug.Log("[Hand Gesture] Left fist + thumb up detected");
+
+        CheckBothThumbsUp();
+    }
+
+    public void OnLeftFistThumbUpEnded()
+    {
+        leftThumbUp = false;
+        Debug.Log("[Hand Gesture] Left fist + thumb up ended");
+    }
+
+    public void OnRightFistThumbUpPerformed()
+    {
+        rightThumbUp = true;
+        Debug.Log("[Hand Gesture] Right fist + thumb up detected");
+
+        CheckBothThumbsUp();
+    }
+
+    public void OnRightFistThumbUpEnded()
+    {
+        rightThumbUp = false;
+        Debug.Log("[Hand Gesture] Right fist + thumb up ended");
+    }
+
+    private void CheckBothFists()
+    {
+        if (leftFist && rightFist)
+        {
+            Debug.Log("[Hand Gesture] BOTH FISTS detected — drone control would start here");
+        }
+    }
+
+    private void CheckBothThumbsUp()
+    {
+        if (leftThumbUp && rightThumbUp)
+        {
+            Debug.Log("[Hand Gesture] BOTH FISTS + BOTH THUMBS UP detected");
+        }
+    }
+}
